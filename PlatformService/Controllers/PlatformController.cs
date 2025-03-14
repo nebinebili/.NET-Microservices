b1859b9ee53a8ddb5409da43bb763340997cf4d9
Nabi Nabili <nbilinbi7@gmail.com>
2025-03-14 18:37:15 +0100

    PLatformService Added

---
+++ PlatformService/Controllers/PlatformController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.DTOs;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformController : ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+
+        private readonly IMapper _mapper;
+
+        public PlatformController(IPlatformRepo repository,IMapper mapper)
+        {
+            _repository=repository;
+            _mapper = mapper;
+        }
+        [HttpGet("GetAllPlatforms")]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetAllPlatforms()
+        {
+            var data=_repository.GetAllPLatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(data));
+        }
+        [HttpGet("GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            if (id > 0) 
+            {
+                var data=_repository.GetPlatformById(id);
+                return data != null ? Ok(_mapper.Map<PlatformReadDto>(data)) : NotFound();
+            }
+
+            return BadRequest("Id is not valid");
+        }
+        [HttpPost("CreatePlatform")]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platform = _mapper.Map<Platform>(platformCreateDto);
+            _repository.CreatePlatform(platform);
+            _repository.SaveChanges();
+
+            var platformReadDto=_mapper.Map<PlatformReadDto>(platform);
+
+            //return CreatedAtRoute(nameof(GetPlatformById),new {Id=platformReadDto.Id},platformReadDto);
+
+            return Ok(platformReadDto);
+        }
+    }
+}
