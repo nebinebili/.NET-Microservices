ae8dcc76076760528ea6e5aa30e888fb478ba651
Nabi Nabili <nbilinbi7@gmail.com>
2025-03-15 01:26:07 +0100

    Command Service Added

--- PlatformService/obj/Debug/net7.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net7.0/PlatformService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1859b9ee53a8ddb5409da43bb763340997cf4d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
